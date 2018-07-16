15d37ef68748af093e11545faac11b6f3adda1b1
Tarik Desire <tprdesire@gmail.com>
2018-07-16 14:49:14 -0400

    Added Error clause

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,13 +1,16 @@
 'use strict';
 
 function yearOfBirth (age) {
-    return 2018 - age;
+  if (age <= 0) {
+    throw new Error('Age cannot be negative');
+  }
+  return 2018 - age;
 }
 
 function whoAmI (name, age) {
-    const yearOB = yearOfBirth(age);
-    console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-    console.log(`I was born in ${yearOfBirth}`);
-};
+  const yearOB = yearOfBirth(age);
+  console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+  console.log(`I was born in ${yearOB}`);
+}
 
-whoAmI('Tarik', 21);
+whoAmI('Tarik', -5);
