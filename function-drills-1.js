5cb320cb3687c6679b0e366ff291c392283cb115
Tarik Desire <tprdesire@gmail.com>
2018-07-16 14:27:06 -0400

    Added Year of Birth

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,6 +1,8 @@
 'use strict';
 function whoAmI (name, age) {
+    let yearOfBirth = 2018 - age;
     console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+    console.log(`I was born in ${yearOfBirth}`);
 };
 
-whoAmI();
+whoAmI('Tarik', 21);
